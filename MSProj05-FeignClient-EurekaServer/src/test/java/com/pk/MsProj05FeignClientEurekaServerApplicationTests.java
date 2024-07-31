95f3fccfad851ed4cbfd801763d0729ed6adb574
pavan <pavan@digiworldexpo.com>
2024-07-31 15:03:12 +0530

    EurekaServer

---
+++ MSProj05-FeignClient-EurekaServer/src/test/java/com/pk/MsProj05FeignClientEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProj05FeignClientEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
